1f13f2a0074353d9ec53f8e80a68d63d9040bfc7
misamoomoo <marvynation@hotmail.com>
2024-04-10 18:51:27 +0300

    Initial commit

---
+++ Source/PuppeteersPurgatory/DebugMacros.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//INCLUDES
+#include "DrawDebugHelpers.h"
+
+// DEFINES
+#define DRAW_RED_SPHERE(Location) if (GetWorld()) \
+			DrawDebugSphere(GetWorld(), Location, 25.f, 6, FColor::Red, true);
+#define DRAW_RED_LINE(StartLocation, EndLocation) if (GetWorld()) \
+			DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
+#define DRAW_RED_POINT(Location) if (GetWorld()) \
+			DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Red, true);
